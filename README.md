7820f79dd1d71da6246bd3d084e4cd60c5955e17
varunkumarkuruva1 <varunkumar27011999@gmail.com>
2021-02-12 13:21:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9h897y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9h897y)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone XL',
+    price: 799,
+    description: 'A large phone with one of the best features'
+  },
+  {
+    id: 2,
+    name: 'Oppo',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'RedMi',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
